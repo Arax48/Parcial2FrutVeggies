993874fa0bb36146608ecf6e14869a94d23efb00
Alejandro Rodriguez <alrodriguezar@unal.edu.co>
2024-08-08 16:53:46 -0500

    First Exam part Completed (Without the extra point)

---
+++ src/mercado/FrutasVerduras.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mercado;
+
+/**
+ *
+ * @author arax4
+ */
+public class FrutasVerduras {
+    
+}
